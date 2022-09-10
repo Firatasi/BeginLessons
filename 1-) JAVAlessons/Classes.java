7af60676ccfb1e40a4e83786ec68f2ea8c229672
Firat <Firat_asi@icloud.com>
2022-09-11 00:01:35 +0300

    nesne tabanlı programlama1

---
+++ 1-) JAVAlessons/Classes.java
@@ -0,0 +1,16 @@
+
+public class Classes {
+
+	public static void main(String[] args) {
+		//Nesen tabanlı programlama
+		//sınıfAdı objeAdı = new SınıfAdı();
+		
+		HspMknsi makine = new HspMknsi();
+		makine.sayi1 = 10;
+		makine.sayi2 = 20;
+		System.out.println(makine.sayi1);
+		
+		
+	}
+
+}
