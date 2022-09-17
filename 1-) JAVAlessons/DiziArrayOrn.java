51a98da55de46772f6cf197f540fd124d295a4d5
Firat <Firat_asi@icloud.com>
2022-09-17 21:46:12 +0300

    Arrays(diziler) ornek

---
+++ 1-) JAVAlessons/DiziArrayOrn.java
@@ -0,0 +1,17 @@
+
+public class DiziArrayOrn {
+
+	public static void main(String[] args) {
+	
+		String[] krediler = {
+				"Emekli Kredi","MEB Kredi","MSB Kredi","Genç Kredi","Öğrenci Kredi"//binlerce veri daha girilebilir
+		};
+		
+		for(String kredi : krediler) {
+			System.out.println(kredi);
+		}
+
+	}
+
+}
+//arrays(diziler) birden çok veriyi kısa yoldan yazdırmak için kullanılabilir
