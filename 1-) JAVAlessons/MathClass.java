48532536c9bd35dcdc55f7634d1df29cc7756673
Firat <Firat_asi@icloud.com>
2022-09-17 14:59:35 +0300

    math class(matematiksel işlem örnekleri...)

---
+++ 1-) JAVAlessons/MathClass.java
@@ -0,0 +1,47 @@
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		//	Math.ceil(x) => Bir büyük sayıya yuvarlar
+		//	Math.cos(x) => Cos değerini hesaplar
+		//	Math.sin(x) => Sin değerini hesaplar
+		//	Math.floor(x) => Bir küçük sayıya yuvarlar
+		//	Math.max(x,y) => Büyük sayıyı yazar
+		//	Math.min(x,y) => Küçük sayıyı yazar
+		//	Math.pow(x,y) => x^y (x üzeri y hesaplar)
+		//	Math.random() => 1 ile 0 arasında rastgele değer yazar
+		//	Math.round(x) => Yakın olan tam sayıya döner
+		//	Math.abs(x) => Mutlak değer alır
+		//...
+		
+		System.out.println(Math.ceil(1.3));
+		System.out.println("------------");
+
+		System.out.println(Math.cos(90));
+		System.out.println("------------");
+
+		System.out.println(Math.sin(90));
+		System.out.println("------------");
+
+		System.out.println(Math.floor(1.4));
+		System.out.println("------------");
+
+		System.out.println(Math.max(4,3));
+		System.out.println("------------");
+
+		System.out.println(Math.min(-1, 5));
+		System.out.println("------------");
+
+		System.out.println(Math.pow(4,8));
+		System.out.println("------------");
+
+		System.out.println(Math.random());
+		System.out.println("------------");
+
+		System.out.println(Math.round(5.6));
+		System.out.println("------------");
+
+		System.out.println(Math.abs(-7));
+	}
+
+}
