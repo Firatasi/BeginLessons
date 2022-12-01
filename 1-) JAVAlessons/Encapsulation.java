76ce544c38c09c521effeb9a9ba57a6418ebe5df
Firat <Firat_asi@icloud.com>
2022-12-01 21:22:10 +0300

    get set

---
+++ 1-) JAVAlessons/Encapsulation.java
@@ -0,0 +1,26 @@
+
+public class Encapsulation {
+
+
+		//Encapsulation (Sarmallama, Kapsülleme)
+		//Inheritance (Kalıtım)
+		//Polymorphism (Çok Biçimlilik)
+		//Abstraction (Soyutlaama)
+		
+		
+		public String name,author,publisher;//ad,yazar,yayıncı
+		public int numberOfPage;
+		
+		
+		Encapsulation(String name,int numberOfPage,String author,String publisher){
+			this.name = name;
+			this.numberOfPage = numberOfPage;
+			this.author = author;
+			this.publisher = publisher;
+			
+		}
+	
+
+}
+
+
