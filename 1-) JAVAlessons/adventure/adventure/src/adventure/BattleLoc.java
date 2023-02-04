ccf05035765bec53cb4223f244239c8ba22a2f5b
Firat <Firat_asi@icloud.com>
2023-02-05 00:21:41 +0300

    adventure

--- 1-) JAVAlessons/adventure/adventure/src/adventure/BattleLoc.java
+++ 1-) JAVAlessons/adventure/adventure/src/adventure/BattleLoc.java
@@ -14,7 +14,46 @@ public abstract class BattleLoc extends Location {
 		System.out.println("Be careful! Here" + obsCount + "unit" + obstacle.getName() + "Two vampires live here!");
 		System.out.println("<W>ar or <E>scape" );
 		String selCase = scan.next();
-		
+		selCase = selCase.toUpperCase();
+		if(selCase.equals("s")) {
+			if (combat(obsCount)) {
+				System.out.println("You defeated the enemies!");
+				return true;
+			}else {
+				System.out.println("You died!");
+				return false;
+			}
+		}
 		return true;
 	}
+	
+	public boolean combat(int obsCount) {
+		for (int i=0; i<obsCount; i++) {
+			playerStats();
+			enemyStats();
+		}
+		return true;
+	
+	}
+	
+	public void playerStats( ) {
+		System.out.println("Player values\n--------");
+		System.out.println("Health: " + player.getHealthy());
+		System.out.println("Damage: " + player.getTotalDamage());
+		System.out.println("Money: " + player.getMoney() );
+		if (player.getInv().getDamage() > 0 ) {
+			System.out.println("Gun: "  + player.getInv().getwName());
+		}
+		if (player.getInv().getArmor() > 0 ) {
+			System.out.println("Armor: "  + player.getInv().getaName());
+		}
+	}
+	
+	public void enemyStats() {
+		System.out.println(obstacle.getNName() + "Values\n----------");
+		System.out.println("Health: " + obstacle.getHealth());
+		System.out.println("Damage: " + obstacle.getDamage());
+		System.out.println("Award: " + obstacle.getAward());
+	}
+	
 }
