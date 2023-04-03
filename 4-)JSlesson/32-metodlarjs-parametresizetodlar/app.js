63d4d2a605661c5756dcf289d1ac9038e860e7b1
Firat <Firat_asi@icloud.com>
2023-04-03 15:04:49 +0300

    jsmethods

---
+++ 4-)JSlesson/32-metodlarjs-parametresizetodlar/app.js
@@ -0,0 +1,21 @@
+// Parametresiz ve geriye değer döndürmeyen metot tanımlamak
+
+/*
+*function yazdir() {
+    //kodlar
+}
+*/
+//hazırlanan metodu...
+function write() {
+    console.log("Fırat");
+}
+
+write(); //çağırır ve çalıştırır
+write(); // kodu(fonksiyonu, metodu) birden fazla yerde calistirabilirsiniz
+
+
+function plus() {
+    console.log(5+7);
+}
+
+plus();
