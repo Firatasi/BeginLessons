49b77d98abfe3f8d69210d983defb6a5bfd9025c
Firat <Firat_asi@icloud.com>
2023-04-05 14:35:58 +0300

    jsparametreli metod

---
+++ 4-)JSlesson/33-Parametrelimetod/app.js
@@ -0,0 +1,31 @@
+//Parametreli metod tanımlamak
+
+function yazdir(isim, soyisim) {   //PARAMETRELİ METOD parantezin içine birseyler yazılırsa parametreli metod olur
+    console.log(isim+ "" + soyisim);
+}
+
+function atak() { //parametresiz metod
+
+}
+
+yazdir("Fırat", "A.");
+
+function cube(sayi) {
+    console.log(sayi*sayi*sayi);
+}
+
+sayi(2);
+sayi(9);
+
+//metodlar kodu bir kere yazıp birden fazla yerde ihtiyaca gore kullanılması ıcın tasaarlanmıstır
+
+
+let yas = Number(prompt("Yaşınızı Giriniz: "));
+
+function denetim() {
+    if(yas>=18) {
+        alert("Ehliyet Alabilirsiniz.");
+    }else {
+        alert("18 Yaşından Küçük Olduğunuz İçin Ehliyet Alamazsınız.")
+    }
+}
