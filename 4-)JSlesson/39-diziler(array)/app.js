7eb4fc47c7c08f12af401e5b7f563f90cce1228f
Firat <Firat_asi@icloud.com>
2023-04-25 18:55:30 +0300

    js array

---
+++ 4-)JSlesson/39-diziler(array)/app.js
@@ -0,0 +1,20 @@
+let numbers = [0,1,2,3,4,"Atakan",5,6,7,8,9,"Fırat"];
+//sayilaron her biri bir indekstir 0. index 1. idex 2. index ...
+
+//8 sayısı icin 9. index cagirilir.
+console.log(numbers[5]);
+
+numbers[6] = "Mahmut"; //atakan ismini mahmutla değştirir
+console.log(numbers[6]);
+
+numbers[numbers.length-1] = "Ali"; //sonuncu elemanı ali ile değiştirir. .length-1 sonuncu elemanı secer.
+
+console.log(numbers); //indexler
+
+
+
+let names = ["Fırat", "Yusuf", "Eda", "Rece"]
+names [2] = "Malik";
+console.log(names[2]);
+
+console.log(names);//indexler
