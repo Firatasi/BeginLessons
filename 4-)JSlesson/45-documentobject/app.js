bd9b926f2c739be62975df179249a9a275f2957a
Firat <Firat_asi@icloud.com>
2023-05-17 17:06:14 +0300

    jsdocumentobject

---
+++ 4-)JSlesson/45-documentobject/app.js
@@ -0,0 +1,13 @@
+//Document
+
+let value;
+
+value = document.location.href;//linki verir.
+value = document.location.hostname;//hos adresini verir.
+value = document.location.port; //portunu verir.
+value = document.location.pathname;//dosya adını verir.
+
+
+value = document.links.item(4);//id sini verir
+value = document.links.item(4).getAttribute("id");//
+console.log(value);
