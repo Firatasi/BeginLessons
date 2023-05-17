6b80e0893e34273173bb878b7136a265b062f14e
Firat <Firat_asi@icloud.com>
2023-05-17 21:19:09 +0300

    js

--- 4-)JSlesson/45-documentobject/app.js
+++ 4-)JSlesson/45-documentobject/app.js
@@ -1,13 +1,30 @@
 //Document
 
-let value;
+// let value;
 
-value = document.location.href;//linki verir.
-value = document.location.hostname;//hos adresini verir.
-value = document.location.port; //portunu verir.
-value = document.location.pathname;//dosya adını verir.
+// value = document.location.href;//linki verir.
+// value = document.location.hostname;//hos adresini verir.
+// value = document.location.port; //portunu verir.
+// value= document.location.pathname;//dosya adını verir.
 
 
-value = document.links.item(4);//id sini verir
-value = document.links.item(4).getAttribute("id");//
-console.log(value);
+// value = document.links.item(4);//id sini verir
+// value = document.links.item(4).getAttribute("id");
+// console.log(valuee);
+
+
+
+
+
+//!2SELECTORS (SEÇİCİLER)- STYLE ÖZELLİKLERİ
+//!CLASSNAME, İD, TAG NAME
+
+//*getElementById : idye göre elementi yakalar
+
+
+
+const button = document.getElementById("todoAddButton");
+
+console.log(button)
+//console.log(button); 
+console.log(button.getAttribute("id"));
