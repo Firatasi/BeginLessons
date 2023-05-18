6838bf9cb760e0c0253772f107aa729add9d70d3
Firat <Firat_asi@icloud.com>
2023-05-18 15:13:55 +0300

    jselementdelete

--- 4-)JSlesson/48-Elementoluşturma/app.js
+++ 4-)JSlesson/48-Elementoluşturma/app.js
@@ -12,3 +12,16 @@ link.innerHTML = "Google";
 cardBody.appendChild(link);
 
 console.log(link);
+
+
+
+//!Element silmek
+
+const todoloist = document.querySelector(".list-group");
+const todos = document.querySelectorAll(".list-group-item");
+
+todos[0].remove();
+todos[1].remove();
+console.log(todo);
+
+//!Element Yer değiştirme
