783c3ec25e77f87c7d58a5c3a30dd297d310cf57
Firat <Firat_asi@icloud.com>
2023-05-23 00:58:44 +0300

    inpt

--- 4-)JSlesson/49-events(olaylar)/app.js
+++ 4-)JSlesson/49-events(olaylar)/app.js
@@ -21,44 +21,106 @@ function changeTitle() {
 //mousedown       **
 
 
-document.addEventListener("DOMContentLoaded",run); //Event atama şekli addevenlistener()-2 sayfa yüklendiğinde run fonksiyonunun çalışmasını sağlar.
+// document.addEventListener("DOMContentLoaded",run); //Event atama şekli addevenlistener()-2 sayfa yüklendiğinde run fonksiyonunun çalışmasını sağlar.
 
-function run() {
-    alert("Page");
-}
+// function run() {
+//     alert("Page");
+// }
 
 
-const cardTitle = document.querySelectorAll(".card-title")[1];
-const cardBody = document.querySelectorAll(".card-body")[1];
+// const cardTitle = document.querySelectorAll(".card-title")[1];
+// const cardBody = document.querySelectorAll(".card-body")[1];
 
-cardTitle.addEventListener("cick", runa);
+// cardTitle.addEventListener("cick", runa);
 
-function runa(e) {
+// function runa(e) {
+//     console.log(e.type);
+// }
+
+
+// document.addEventListener("dbclick",runc); 
+
+// function runc(e) {
+//     alert(e.type);
+// }
+
+// document.addEventListener("mouseover",rund); 
+
+// function rund(e) {
+//     console.log(e.type);
+// }
+
+// document.addEventListener("mouseout",rune); 
+
+// function rune(e) {
+//     console.log(e.type);
+// }
+
+// document.addEventListener("mouseenter",rund); 
+
+// function rund(e) {
+//     console.log(e.type);
+// }
+
+
+//Klavye Eventleri
+
+//keypress  harf ve sayılarda tetiklenen event türüdür
+
+//keydown   harf,sayı ve klavyedeki diğer tuşlarıyle çalışanevent türüdür.
+
+//keyup     klavyeden basılan tusa basmayı bıraktıgımızda çalısır
+
+
+
+// document.addEventListener("keypress",runx);
+
+// function runx(e) {
+//     console.log(e.type);
+//     //console.loog(e.key);
+//     //console.loog(e.keyCode);
+//     //console.loog(e.keywhich);
+// }
+
+// document.addEventListener("keydown",runy);
+
+// function runy(e) {
+//     console.log(e.type);
+// }
+
+// document.addEventListener("keyup",runz);
+
+// function runz(e) {
+//     console.log(e.type);
+// }
+
+//!İNPUT EVENTLERİ
+
+//focus  inputun içine girildiğinde çalışır
+//blur   inputtan çıkıldığında çalışır
+//copy   input kopyalandığında çalışır
+//paste  inputa bir veri yapıştırıldığında çalışır
+//cut    inptta kesme olursa çalışır
+//select inputtaki veride seçilen yer olursa çalışır
+
+const todo = document.querySelectorAll("#todoName");
+
+todo.addEventListener("focus",runm);
+todo.addEventListener("blur",runm);
+todo.addEventListener("copy",runk);
+todo.addEventListener("paste",runl);
+
+function runm(e) {
     console.log(e.type);
 }
 
 
-document.addEventListener("dbclick",runc); 
-
-function runc(e) {
-    alert(e.type);
-}
-
-document.addEventListener("mouseover",rund); 
-
-function rund(e) {
+function runk(e) {
     console.log(e.type);
 }
 
-document.addEventListener("mouseout",rune); 
-
-function rune(e) {
+function runl(e) {
     console.log(e.type);
 }
 
-document.addEventListener("mouseenter",rund); 
-
-function rund(e) {
-    console.log(e.type);
-}
 
